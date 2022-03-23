fe33aaf642d22c73a0b1282222a4f2b734646d23
knrichter19 <knrichter19@coe.edu>
2022-03-23 16:35:42 -0500

    evil

--- src/edu/coe/cs245/Main.java
+++ src/edu/coe/cs245/Main.java
@@ -2,8 +2,9 @@ package edu.coe.cs245;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 	// This is some code
-        
+        System.out.println("Broken!");
+        return 1;
     }
 }
